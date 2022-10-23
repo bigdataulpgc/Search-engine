27387bc40d56af5a4c0397890b510349fd46a586
Joeel71 <elnanito2002@gmail.com>
2022-10-23 11:48:45 +0100

    MetadataExtractor test

--- DocumentBuilder/DocumentBuilder/src/main/java/builder/MetadataExtractor.java
+++ DocumentBuilder/DocumentBuilder/src/main/java/builder/MetadataExtractor.java
@@ -1,16 +1,42 @@
 package builder;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+
 public class MetadataExtractor {
 
     public String text;
+    public Metadata metadata;
 
-    public MetadataExtractor(String text) {
+    public MetadataExtractor(String text) throws InvocationTargetException, IllegalAccessException {
         this.text = text;
+        this.metadata = new Metadata();
+        for (Method method: this.getClass().getDeclaredMethods())
+            System.out.println(method);
     }
 
-    public Document extract(){
+    public Metadata getMetadata() {
+        return this.metadata;
+    }
 
+    public void extractTitle(){
 
-        return null;
+    }
+
+    public void extractAuthors(){
+
+    }
+
+    public void extractReleaseDate(){
+
+    }
+
+    public void extractPostingDate(){
+
+    }
+
+    public void extractLanguage(){
+
     }
 }
--- DocumentBuilder/DocumentBuilder/src/test/java/tests/should_check_extraction_of_metadata.java
+++ DocumentBuilder/DocumentBuilder/src/test/java/tests/should_check_extraction_of_metadata.java
@@ -3,10 +3,14 @@ package tests;
 import builder.Document;
 import builder.Metadata;
 import builder.MetadataExtractor;
+import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import utils.UrlReader;
 
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 public class should_check_extraction_of_metadata {
 
@@ -19,6 +23,23 @@ public class should_check_extraction_of_metadata {
             throw new RuntimeException(e);
         }
 
-        Document document = new MetadataExtractor(text).extract();
+        Metadata metadata;
+
+        try {
+            metadata = new MetadataExtractor(text).getMetadata();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        Assertions.assertThat(metadata.getTitle()).isEqualTo("A bird of passage");
+        Assertions.assertThat(metadata.getAuthors().get(0)).isEqualTo("Bithia Mary Croker");
+        Assertions.assertThat(metadata.getLanguage()).isEqualTo("English");
+        SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy");
+        try {
+            Assertions.assertThat(metadata.getAuthors()).isEqualTo(formato.parse("21/11/2022"));
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+        Assertions.assertThat(metadata.getAuthors()).isNull();
     }
 }
