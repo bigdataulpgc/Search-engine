bab2578c759e955ed8ea5323e70ae7b029e9cd11
Joeel71 <elnanito2002@gmail.com>
2022-10-28 08:58:19 +0100

    actual version DocumentBuilder

--- DocumentBuilder/DocumentBuilder/src/main/java/builder/DocumentBuilder.java
+++ DocumentBuilder/DocumentBuilder/src/main/java/builder/DocumentBuilder.java
@@ -1,24 +1,6 @@
 package builder;
 
-import java.lang.reflect.InvocationTargetException;
+public interface DocumentBuilder {
 
-public class DocumentBuilder {
-
-    public String text;
-
-    public DocumentBuilder(String text){
-        this.text = text;
-    }
-
-    public Document build() throws InvocationTargetException, IllegalAccessException {
-        Document document = new Document();
-        Metadata metadata = new MetadataExtractorGutenberg(this.text).getMetadata();
-        document.setMetadata(metadata).setContent(this.extractContent());
-        return document;
-    }
-
-    public String extractContent() {
-        String content = this.text.split("\\*\\*\\*")[2];
-        return content;
-    }
+    Document build() throws Exception;
 }
